ded114392560d350c8ea7e689b4e0a89fa62493a
Hizuro <hizuro@gmx.net>
2015-01-11 09:28:56 +0100

    remove under_construction folder from repository

--- modules/under_construction/reputation.lua
+++
@@ -1,7 +0,0 @@
---[[
-ideas:
-	default - list of all active factions with reputation and session plus
-	fav mode - list of factions choosed by user
-
-	mouseover - tooltip with list of other user characters with count.
-]]
