f73ff3315b33572d38ddadd47f361e41f42fe831
Hizuro <hizuro@gmx.net>
2015-06-25 01:06:07 +0200

    speed: adding tooltip with learned riding skills, speed bonus (by race, class and guild) and flight licences.

--- modules/speed.lua
+++ modules/speed.lua
@@ -14,22 +14,37 @@ local ldbName, ttName = name, name.."TT"
 local tt = nil
 local string,GetUnitSpeed,UnitInVehicle = string,GetUnitSpeed,UnitInVehicle
 local ttColumns = 2;
-local riding_skills = { -- <spellid>, <skill>, <minLevel>, <speed increase>
+local riding_skills = { -- <spellid>, <skill>, <minLevel>, <air speed increase>, <ground speed increase>
 	{90265, 300, 80, 310},
 	{34091, 225, 70, 280},
 	{34090, 150, 60, 150},
 	{33391,  75, 40, 100},
-	{33388,  50, 20, 60},
+	{33388,  50, 20,  60},
 };
 local licences = { -- <spellid>, <minLevel>, <mapIds>
 	{90267,  60, {[4]=1,[9]=1,[11]=1,[13]=1,[14]=1,[16]=1,[17]=1,[19]=1,[20]=1,[21]=1,[22]=1,[23]=1,[24]=1,[26]=1,[27]=1,[28]=1,[29]=1,[30]=1,[32]=1,[34]=1,[35]=1,[36]=1,[37]=1,[38]=1,[39]=1,[40]=1,[41]=1,[42]=1,[43]=1,[61]=1,[81]=1,[101]=1,[121]=1,[141]=1,[161]=1,[181]=1,[182]=1,[201]=1,[241]=1,[261]=1,[281]=1,[301]=1,[321]=1,[341]=1,[362]=1,[381]=1,[382]=1,[462]=1,[463]=1,[464]=1,[471]=1,[476]=1,[480]=1,[499]=1,[502]=1,[545]=1,[606]=1,[607]=1,[610]=1,[611]=1,[613]=1,[614]=1,[615]=1,[640]=1,[673]=1,[684]=1,[685]=1,[689]=1,[700]=1,[708]=1,[709]=1,[720]=1,[772]=1,[795]=1,[864]=1,[866]=1,[888]=1,[889]=1,[890]=1,[891]=1,[892]=1,[893]=1,[894]=1,[895]=1}},
 	{54197,  68, {[485]=1,[486]=1,[510]=1,[504]=1,[488]=1,[490]=1,[491]=1,[541]=1,[492]=1,[493]=1,[495]=1,[501]=1,[496]=1}},
-	{115913, 85, {[862]=1,[858]=1,[929]=1,[928]=1,[857]=1,[809]=1,[905]=1,[903]=1,[806]=1,[873]=1,[808]=1,[951]=1,[810]=1,[811]=1,[807]=1}}
+	{115913, 85, {[862]=1,[858]=1,[929]=1,[928]=1,[857]=1,[809]=1,[905]=1,[903]=1,[806]=1,[873]=1,[808]=1,[951]=1,[810]=1,[811]=1,[807]=1}},
+	{"a10018",    90, {}},
+}
 
+local bonus_spells = { -- <spellid>, <chkActive[bool]>, <type>, <typeValue>, <customText>, <speed increase>, <special>
+	-- race spells
+	{154748,  true, "race", "NIGHTELF", true,  1, {"TIME", {18,24}, {0,6}} },
+	{ 20582,  true, "race", "NIGHTELF", true,  2},
+	{ 20585,  true, "race", "NIGHTELF", true, 75, {"SPELL", 8326}},
+	{ 68992,  true, "race",     "WORG", true, 40},
+	{ 69042,  true, "race",   "GOBLIN", true,  1},
+
+	-- class spells
+	-- class: druid, id: 11
+	{  5215,  true, "class",        11, true, 30},
+	-- glyphes
+
+	-- misc
+	{ 78633, false,    nil,        nil, true, 10}
 }
-local bonus_spells = {
-	
-}
+
 
 -------------------------------------------
 -- register icon names and default files --
@@ -62,6 +77,7 @@ ns.modules[name] = {
 -- some local functions --
 --------------------------
 local function createTooltip(tt)
+	local _=function(d) return ("+%d%%"):format(d); end;
 	tt:Clear();
 
 	tt:AddHeader(C("dkyellow",L[name]));
@@ -77,40 +93,116 @@ local function createTooltip(tt)
 		end
 		if (learned==nil) then
 			if(UnitLevel("player")>=v[3])then
-				tt:AddLine(C("yellow",Name), "+"..v[4].."%");
+				tt:AddLine(C("yellow",Name), _(v[4]));
 			else
-				tt:AddLine(C("red",Name), "+"..v[4].."%");
+				tt:AddLine(C("red",Name), _(v[4]));
 			end
 		elseif(learned==true)then
-			tt:AddLine(C("green",Name), "+"..v[4].."%");
+			tt:AddLine(C("green",Name), _(v[4]));
 			learned=false;
 		elseif(learned==false)then
-			tt:AddLine(C("dkgreen",Name), "+"..v[4].."%");
+			tt:AddLine(C("dkgreen",Name), _(v[4]));
 		end
 	end
 
-	tt:AddSeparator(4,0,0,0,0);
-	tt:AddLine(C("ltblue",L["Flight licences"]));
-	tt:AddSeparator();
-	for i,v in ipairs(licences) do
-		local Name, rank, icon, castTime, minRange, maxRange, spellId = GetSpellInfo(v[1]);
-		if(IsSpellKnown(v[1])) then
-			tt:AddLine(C("green",Name));
-		else
-			tt:AddLine(C("red",Name));
-		end
+	if (UnitLevel("player")<20)then
+		tt:AddLine(C("orange","You must be reach level 20 to learn riding."));
 	end
 
-	--[[
 	tt:AddSeparator(4,0,0,0,0);
 	tt:AddLine(C("ltblue",L["Speed bonus"]));
 	tt:AddSeparator();
-	-- by race
-	-- by class
-		-- hunter
-		-- paladin
-	-- by item
-	]]
+	local Id,ChkActive,Type,TypeValue,CustomText,Speed,Special=1,2,3,4,5,6,7;
+	for i,spell in ipairs(bonus_spells)do
+
+		local id = nil;
+		if(spell[Type]=="race")then
+			if(spell[TypeValue]==ns.player.race:upper())then
+				id = spell[Id];
+			end
+		elseif(spell[Type]=="class")then
+			if(spell[TypeValue]==ns.player.classId)then
+				id = spell[Id];
+			end
+		else
+			id = spell[Id];
+		end
+
+		if(id and IsSpellKnown(id))then
+			local active=false;
+			local custom = "";
+			local Name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell[Id]);
+
+			if(spell[CustomText]==true)then
+				rank = {strsplit(" ",rank)};
+				spell[CustomText] = rank[2] or rank[1];
+			end
+
+			if(spell[CustomText])then
+				custom = " "..C("ltgray","("..spell[CustomText]..")");
+			end
+
+			if(spell[ChkActive])then
+				local start, duration, enabled = GetSpellCooldown(spell[Id]);
+				if(spell[Special])then
+					if(spell[Special][1]=="SPELL")then
+						local n = GetSpellInfo(spell[Special][2]);
+						if(UnitDebuff("player",n))then
+							active=true;
+						end
+					elseif(spell[Special][1]=="TIME")then
+						local h = GetGameTime();
+						for i,v in ipairs(spell[Special])do
+							if(type(v)=="table" and h>=v[1] and h<=v[2])then
+								active=true;
+							end
+						end
+					end
+				elseif(enabled)then
+					active=true;
+				end
+			elseif(spell[Special])then
+				
+			else
+				active=true;
+			end
+
+			if(active)then
+				tt:AddLine(C("ltyellow",Name .. custom), _(spell[Speed]));
+			end
+		end
+	end
+	--- inventory items ?
+
+
+	if (UnitLevel("player")>=20)then
+		tt:AddSeparator(4,0,0,0,0);
+		tt:AddLine(C("ltblue",L["Flight licences"]));
+		tt:AddSeparator();
+		for i,v in ipairs(licences) do
+			local Name, rank, icon, castTime, minRange, maxRange, completed, wasEarnedByMe, ready, _;
+			if(type(v[1])=="string")then
+				local id = tonumber(v[1]:match("a(%d+)"));
+				if(id)then
+					_, Name, _, completed, _, _, _, _, _, _, _, _, wasEarnedByMe = GetAchievementInfo(id);
+					ready = completed;
+				end
+			else
+				Name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(v[1]);
+				ready = IsSpellKnown(v[1]);
+			end
+			if(Name)then
+				if(ready) then
+					tt:AddLine(C("green",Name));
+				elseif(UnitLevel("player")>=v[2])then
+					tt:AddLine(C("yellow",Name));
+				else
+					tt:AddLine(C("red",Name));
+				end
+			end
+		end
+	end
+
 end
 
 ------------------------------------
