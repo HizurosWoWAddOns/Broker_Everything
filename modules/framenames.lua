0177ccbdadb00c616b3c4359e5a135fea2838286
Hizuro <hizuro@gmx.net>
2016-01-14 16:29:40 +0100

    framenames: added ownership of elements by hold shift key and removed unused code

--- modules/framenames.lua
+++ modules/framenames.lua
@@ -10,9 +10,7 @@ local C, L, I = ns.LC.color, ns.L, ns.I
 -- module own local variables and local cached functions --
 -----------------------------------------------------------
 local name = "Framenames" -- L["Framenames"]
-local ldbName = name
-local tt = nil
-local ttName = name.."TT"
+local ldbName,ttName,ldbObject = name,name.."TT"
 local string = string
 
 
@@ -39,49 +37,6 @@ ns.modules[name] = {
 --------------------------
 -- some local functions --
 --------------------------
-local lastFrame = nil
-local function FrameInfoTooltip(tt,frame)
-	if lastFrame==frame then return end
-
-	tt:Clear()
-
-	if frame:IsForbidden() or ( frame:IsProtected() and InCombatLockdown() ) then
-		tt:AddLine(L["Name"], (frame:IsForbidden() and "[Forbidden Frame]") or (frame:IsProtected() and "[Protected Frame]") or "[Unknown]")
-	else
-		tt:AddHeader(frame:GetName() or "<anonym>")
-		tt:AddSeparator()
-		local tmp
-		for i,v in pairs(frame) do
-			if i~=0 and ( type(v)=="string" or type(v)=="number" ) then
-				if strlen(v)>26 then v = strsub(v,0,23).."..." end
-				tt:AddLine(i, v);
-			end
-		end
-		tmp = frame:GetParent()
-		tt:AddLine("GetParent", (tmp~=nil and tmp:GetName()) or "nil <anonym?>")
-	end
-
-	if lastFrame~=frame then
-		lastFrame = frame
-	end
-end
-
-local lastUnit = nil
-local function UnitInfoTooltip(tt)
-	local tmp
-	local guid = UnitGUID("mouseover")
-	local name = UnitName("mouseover")
-	if lastUnit==name then return end
-
-	tt:Clear()
-
-	tt:AddLine("UnitName",name)
-	tt:AddLine("UnitGUID",guid or "Unknown")
-
-	if lastUnit~=guid then
-		lastUnit = name
-	end
-end
 
 
 ------------------------------------
@@ -90,66 +45,50 @@ end
 ns.modules[name].init = function(self)
 	ldbName = (Broker_EverythingDB.usePrefix and "BE.." or "")..name
 	if self then
-		local dataobj = self.obj or ns.LDB:GetDataObjectByName(ldbName)
-		dataobj.text = L[name]
+		ldbObject = ldbObject or ns.LDB:GetDataObjectByName(ldbName);
+		ldbObject.text = L[name];
 	end
 end
 
 -- ns.modules[name].onevent = function(self,event,msg) end
 
 ns.modules[name].onupdate = function(self)
-	local dataobj = self.obj or ns.LDB:GetDataObjectByName(ldbName)
+	ldbObject = ldbObject or ns.LDB:GetDataObjectByName(ldbName);
 	local F = nil
 	local f = GetMouseFocus()
 	if (not f) then
-		if dataobj.text~=L["Unknown"] then
-			dataobj.text = L["Unknown"]
+		if ldbObject.text~=L["Unknown"] then
+			ldbObject.text = L["Unknown"]
 		end
 	else
-		F = (f:IsForbidden() and "[Forbidden Frame]") or (f:IsProtected() and InCombatLockdown() and "[Protected Frame]") or f:GetName()
-		if F == nil and type(f.key)=="string" then -- LibQTip tooltips returns nil on GetName but f.key contains the current name
-			F = f.key
-		end
-		if F == nil then
-			F = "<anonym>";
-			for i,v in pairs(f:GetParent())do
-				if(v==f)then
-					F = "(parentKey) "..i;
+		if f:IsForbidden() then
+			F = "[Forbidden Frame]";
+		elseif f:IsProtected() and InCombatLockdown() then
+			F = "[Protected Frame]";
+		else
+			F = f:GetName();
+			if F == nil and type(f.key)=="string" then -- LibQTip tooltips returns nil on GetName but f.key contains the current name
+				F = f.key
+			end
+			if F == nil then
+				F = "<anonym>";
+				for i,v in pairs(f:GetParent() or {})do
+					if(v==f)then
+						F = "(parentKey) "..i;
+					end
+				end
+			end
+			if f:GetName() then
+				local secure, taint = issecurevariable(_G,f:GetName());
+				if IsShiftKeyDown() then
+					F = "[" .. (secure and "Blizzard" or taint) .. "] " .. F;
 				end
 			end
 		end
-		if dataobj.text ~= F then
-			dataobj.text = F
+		if ldbObject.text ~= F then
+			ldbObject.text = F
 		end
 	end
-	--[[
-	if ( f ) and  IsControlKeyDown() and IsAltKeyDown() then
-		if tt==nil then
-			tt = ns.LQT:Acquire(ttName, 2, "LEFT", "RIGHT")
-		elseif tt.key~=ttName then
-			return;
-		end
-
-		if UnitName("mouseover") then
-			UnitInfoTooltip(tt)
-		else
-			FrameInfoTooltip(tt,GetMouseFocus())
-		end
-
-		local s = UIParent:GetEffectiveScale()
-		local x,y = GetCursorPosition()
-		local w,h = tt:GetWidth()/s, tt:GetHeight()/s
-		tt:SetPoint("BOTTOMLEFT",UIParent,"BOTTOMLEFT",(x/s)+12,(y/s)-h)
-		tt:SetClampedToScreen(true)
-
-		if not tt:IsShown() then tt:Show() end
-	elseif tt~=nil and tt.key==ttName then
-		tt:ClearAllPoints()
-		tt:Hide()
-		lastFrame = nil
-		lastUnit = nil
-	end
-	]]
 end
 
 -- ns.modules[name].optionspanel = function(panel) end
