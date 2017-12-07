68e748f322b82dce3d78fb71cdbad6efc0910c54
Hizuro <hizuro@gmx.net>
2017-12-07 14:04:40 +0100

    chatchannels: improved channel user count update

--- modules/chatchannels.lua
+++ modules/chatchannels.lua
@@ -3,15 +3,15 @@
 ----------------------------------
 local addon, ns = ...
 local C, L, I = ns.LC.color, ns.L, ns.I
-local channels={}
-local channels_last =1;
+
 
 -- module own local variables and local cached functions --
 -----------------------------------------------------------
 local name = "ChatChannels";
 local ttName,ttColumns,tt,module,ticker = name.."TT",2
-local iName, iHeader, iCollapsed, iChannelNumber, iCount, iActive, iCategory, iVoiceEnabled, iVoiceActive = 1,2,3,4,5,6,7,8,9;
-local events={PLAYER_LOGIN=1,CHANNEL_UI_UPDATE=1,PARTY_LEADER_CHANGED=1,GROUP_ROSTER_UPDATE=1,CHANNEL_ROSTER_UPDATE=1,MUTELIST_UPDATE=2,IGNORELIST_UPDATE=2}
+local iName, iHeader, iCollapsed, iChannelNumber, iCount, iActive, iCategory, iVoiceEnabled, iVoiceActive = 1,2,3,4,5,6,7,8,9; -- GetChannelDisplayInfo indexes
+local iLastUpdate, iNoUpdate = 10,11; -- custom indexes
+local channels,ChanIndex,updateChannelListLock,updateCountTicker={},0,false;
 local wd = ({
 	enUS="WorldDefense",
 	enBG="WorldDefense",
@@ -37,6 +37,21 @@ I[name] = {iconfile="Interface\\chatframe\\ui-chatwhispericon",coords={0.05,0.95
 
 -- some local functions --
 --------------------------
+local function updateBroker()
+	local obj = ns.LDB:GetDataObjectByName(module.ldbName);
+	local txt={};
+	for i,v in ipairs(channels)do
+		if not v[iHeader] then
+			local color,count = {.5,.5,.5},v[iCount] or 0;
+			if(v.color and v[iActive])then
+				color = v.color;
+			end
+			tinsert(txt,C(color,ns.FormatLargeNumber(name,count)));
+		end
+	end
+	obj.text = #txt>0 and table.concat(txt,"/") or CHAT_CHANNELS;
+end
+
 local function addChannel(tbl,index)
 	local data = {GetChannelDisplayInfo(index)};
 	local chatTypeName = "SYSTEM";
@@ -58,33 +73,43 @@ local function addChannel(tbl,index)
 			data[iActive]=false;
 		end
 	end
-	if(data[iName]==wd)then
-		data.noUpdate = true;
-	end
 	local r,g,b = GetMessageTypeColor(chatTypeName);
 	data.color = {r or 0.6, g or 0.6, b or 0.6};
-	if not (tbl==channels and channels[index] and channels[index][iName]==data[iName]) then
-		data.lastUpdate=0;
+	if data[iName]==wd then
+		-- exclude world defence channel from user count update.
+		-- SetSelectedDisplayChannel on world defence results in error message
+		data[iNoUpdate] = true;
 	end
-	tinsert(tbl,data);
+	if not tbl==channels then
+		data[iLastUpdate] = (data[iCount]>0 and time()) or 0;
+	end
+	tbl[index]=data;
 end
 
-local function updateChannels(index,num)
-	if channels[index]==nil then
-		addChannel(channels,index);
+function nextChannel()
+	repeat
+		ChanIndex=ChanIndex+1;
+	until (channels[ChanIndex] and not channels[ChanIndex][iHeader] and channels[ChanIndex][iActive]) or not channels[ChanIndex];
+
+	if not channels[ChanIndex] then
+		updateCountTicker:Cancel();
+		updateCountTicker = nil;
+	elseif channels[ChanIndex] and channels[ChanIndex][iActive] then
+		SetSelectedDisplayChannel(ChanIndex); -- trigger CHANNEL_COUNT_UPDATE
 	end
-	channels[index][5]=num;
-	channels[index].lastUpdate=time();
 end
 
---local iName, iHeader, iCollapsed, iChannelNumber, iCount, iActive, iCategory, iVoiceEnabled, iVoiceActive = 1,2,3,4,5,6,7,8,9;
-local function updateList()
+local function updateChannelList()
 	local tmp = {};
 	local num = GetNumDisplayChannels();
 	for index=1, num do
 		addChannel(tmp,index);
 	end
 	channels = tmp;
+	updateChannelListLock = false;
+
+	updateBroker();
+	module.onupdate();
 end
 
 local function createTooltip(tt,update)
@@ -128,47 +153,12 @@ local function createTooltip(tt,update)
 	end
 end
 
-local function updater()
-	if(ChannelFrame and ChannelFrame:IsShown()) then return end
-	local I=0;
-	for i,v in ipairs(channels)do
-		if not v.noUpdate and not v[iHeader] and v[iActive] then
-			C_Timer.After(i*.4,function()
-				if(ChannelFrame and ChannelFrame:IsShown()) then return end
-				SetSelectedDisplayChannel(i);
-				if(tt and tt.key and tt.key==ttName)then
-					createTooltip(tt,true);
-				end
-			end);
-		end
-		I=i+1;
-	end
-	C_Timer.After(I*.4,function()
-		local obj = ns.LDB:GetDataObjectByName(module.ldbName);
-		local txt={};
-		for i,v in ipairs(channels)do
-			if not v[iHeader] then
-				local color,count = {.5,.5,.5},v[iCount] or 0;
-				if(v.color and v[iActive])then
-					color = v.color;
-				end
-				tinsert(txt,C(color,ns.FormatLargeNumber(name,count)));
-			end
-		end
-		if(#txt>0)then
-			obj.text = table.concat(txt,"/");
-		else
-			obj.text = CHAT_CHANNELS;
-		end
-	end);
-end
-
 
 -- module functions and variables --
 ------------------------------------
 module = {
 	events = {
-		"PLAYER_LOGIN",
+		"PLAYER_ENTERING_WORLD",
 		"CHANNEL_UI_UPDATE",
 		"CHANNEL_COUNT_UPDATE",
 		"CHANNEL_ROSTER_UPDATE"
@@ -202,22 +192,29 @@ end
 
 -- function module.init() end
 
-function module.onevent(self,event,arg1,arg2,...)
+function module.onevent(self,event,...)
 	local msg = ...;
 	if event=="BE_UPDATE_CFG" and msg and msg:find("^ClickOpt") then
 		ns.ClickOpts.update(name);
 	elseif event=="CHANNEL_COUNT_UPDATE" then
-		updateChannels(arg1,arg2 or 0)
-	end
-	if events[event]==1 then
-		updateList()
-		if not ticker and event=="PLAYER_LOGIN" then
-			C_Timer.After(5,updater);
-			ticker = C_Timer.NewTicker(module.updateinterval,updater);
+		local index, count = ...;
+		if channels[index]==nil then
+			addChannel(channels,index);
 		end
-	elseif event=="d" then
-		updateList()
+		channels[index][iCount] = count or 0;
+		channels[index][iLastUpdate] = time(); -- ?
+		updateBroker();
+	elseif event=="PLAYER_ENTERING_WORLD" or event=="CHANNEL_UI_UPDATE" then
+		if not updateChannelListLock then -- catch multible triggered events
+			C_Timer.After(.15, updateChannelList);
+		end
 	end
+end
+
+function module.onupdate()
+	if (ChannelFrame and ChannelFrame:IsShown()) or (updateCountTicker~=nil) then return end
+	ChanIndex = 0;
+	updateCountTicker = C_Timer.NewTicker(.5, nextChannel);
 end
 
 -- function module.optionspanel(panel) end
