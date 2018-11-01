b3a8b640589dfdb2d02a9b1726f3f35fcaf87002
Hizuro <hizuro@gmx.net>
2018-11-01 11:29:22 +0100

    core: added hidden module option to module registration; some minor tweaks in module registration

--- modules.lua
+++ modules.lua
@@ -6,6 +6,7 @@ ns.modules = {};
 setmetatable(ns.modules,{
 	__newindex = function(t,name,data)
 		rawset(t,name,data);
+		if data.isHiddenModule==true then return end
 		-- first step... register defaults
 		if data.config_defaults and data.config_defaults.minimap==nil then
 			data.config_defaults.minimap = {hide=false};
@@ -37,71 +38,71 @@ function ns.toggleMinimapButton(modName,setValue)
 	end
 end
 
-local function OnEventWrapper(self,...)
-	self.onevent(self.eventFrame,...);
-end
-
 local function moduleInit(name)
 	local mod = ns.modules[name];
 
 	-- register options
-	ns.Options_AddModuleOptions(name);
+	if not mod.isHiddenModule then
+		ns.Options_AddModuleOptions(name);
+	end
 
-	if ns.profile[name].enabled==true then
+	if ns.profile[name].enabled==true or mod.isHiddenModule then
 		-- module init
 		if mod.init then
 			mod.init();
 			mod.init = nil;
 		end
 
-		-- new clickOptions system
-		local onclick;
-		if (type(mod.clickOptions)=="table") then
-			local active = ns.ClickOpts.update(name);
-			if active then
-				function onclick(self,button)
-					ns.ClickOpts.func(self,button,name);
+		if not mod.isHiddenModule then
+			-- new clickOptions system
+			local onclick;
+			if (type(mod.clickOptions)=="table") then
+				local active = ns.ClickOpts.update(name);
+				if active then
+					function onclick(self,button)
+						ns.ClickOpts.func(self,button,name);
+					end
 				end
+			elseif (type(mod.onclick)=="function") then
+				onclick = mod.onclick;
 			end
-		elseif (type(mod.onclick)=="function") then
-			onclick = mod.onclick;
+
+			-- LDB init
+			if (not mod.onenter) and mod.ontooltip then
+				mod.ontooltipshow = mod.ontooltip;
+			end
+
+			local icon = ns.I(name .. (mod.icon_suffix or ""));
+			local iColor = ns.profile.GeneralOptions.iconcolor;
+			mod.ldbName = (ns.profile.GeneralOptions.usePrefix and "BE.." or "")..name;
+			mod.obj = ns.LDB:NewDataObject(mod.ldbName, {
+				-- button data
+				type          = "data source",
+				label         = L[name],
+				text          = L[name],
+				icon          = icon.iconfile, -- default or custom icon
+				staticIcon    = icon.staticIcon or icon.iconfile, -- default icon only
+				iconCoords    = icon.coords or {0, 1, 0, 1},
+
+				-- button event functions
+				OnEnter       = mod.onenter or nil,
+				OnLeave       = mod.onleave or nil,
+				OnClick       = onclick,
+				OnTooltipShow = mod.ontooltipshow or nil,
+
+				-- let user know who registered the broker
+				-- displayable by broker dispay addons...
+				-- DataBrokerGroups using it in option panel.
+				parent        = addon
+			});
+
+			ns.updateIconColor(name);
+
+			-- register minimap button on demand
+			ns.toggleMinimapButton(name);
 		end
 
-		-- LDB init
-		if (not mod.onenter) and mod.ontooltip then
-			mod.ontooltipshow = mod.ontooltip;
-		end
-
-		local icon = ns.I(name .. (mod.icon_suffix or ""));
-		local iColor = ns.profile.GeneralOptions.iconcolor;
-		mod.ldbName = (ns.profile.GeneralOptions.usePrefix and "BE.." or "")..name;
-		mod.obj = ns.LDB:NewDataObject(mod.ldbName, {
-			-- button data
-			type          = "data source",
-			label         = L[name],
-			text          = L[name],
-			icon          = icon.iconfile, -- default or custom icon
-			staticIcon    = icon.staticIcon or icon.iconfile, -- default icon only
-			iconCoords    = icon.coords or {0, 1, 0, 1},
-
-			-- button event functions
-			OnEnter       = mod.onenter or nil,
-			OnLeave       = mod.onleave or nil,
-			OnClick       = onclick,
-			OnTooltipShow = mod.ontooltipshow or nil,
-
-			-- let user know who registered the broker
-			-- displayable by broker dispay addons...
-			-- DataBrokerGroups using it in option panel.
-			parent        = addon
-		});
-
-		ns.updateIconColor(name);
-
-		-- register minimap button on demand
-		ns.toggleMinimapButton(name);
-
-		-- event/update handling
+		-- event handling
 		if (mod.onevent and mod.events) or (mod.onupdate) then
 			if not mod.noEventFrame then
 				mod.eventFrame=CreateFrame("Frame");
@@ -111,14 +112,13 @@ local function moduleInit(name)
 			mod.eventFrame.modName = name;
 			if type(mod.onevent)=="function" and type(mod.events)=="table" then
 				mod.eventFrame:SetScript("OnEvent",mod.onevent);
-				mod.OnEvent = OnEventWrapper;
-				mod:OnEvent("BE_UPDATE_CFG");
-				mod:OnEvent("BE_UPDATE_CFG","ClickOpt");
+				mod.onevent(mod.eventFrame,"BE_UPDATE_CFG");
+				mod.onevent(mod.eventFrame,"BE_UPDATE_CFG","ClickOpt");
 				for _, e in pairs(mod.events) do
-					if e=="ADDON_LOADED" then
-						mod:OnEvent(e,addon);
-					elseif e=="PLAYER_LOGIN" and ns.eventPlayerEnteredWorld then -- for later enabled modules
-						mod:OnEvent(e);
+					if e=="ADDON_LOADED" or (e=="ADDON_LOADED" and ns.eventPlayerEnteredWorld) then
+						mod.onevent(mod.eventFrame,e,addon);
+					elseif e=="PLAYER_LOGIN" or (e=="PLAYER_LOGIN" and ns.eventPlayerEnteredWorld) then
+						mod.onevent(mod.eventFrame,e);
 					end
 					mod.eventFrame:RegisterEvent(e);
 				end
@@ -131,7 +131,7 @@ local function moduleInit(name)
 		end
 
 		-- onupdate function
-		-- frame script OnUpdate ticking to too fast... C_Timer is better
+		-- frame script OnUpdate ticking too fast... C_Timer is better
 		if type(mod.onupdate)=="function" and type(mod.onupdate_interval)=="number" and mod.onupdate_interval>0 then
 			mod.onupdate_ticker = C_Timer.NewTicker(mod.onupdate_interval,mod.onupdate);
 		end
@@ -139,7 +139,7 @@ local function moduleInit(name)
 		-- chat command registration
 		if (mod.chatcommands) then
 			for k,v in pairs(mod.chatcommands) do
-				if (type(k)=="string") then -- prevents overriding
+				if (type(k)=="string") then
 					ns.AddChatCommand(k,v);
 					-- ns.AddChatCommand("<string>",{desc="<string>",func=function()end});
 				end
