4a668d967a1eb3cd2f996d98b7bcceff63128077
Hizuro <hizuro@gmx.net>
2018-11-16 03:58:30 +0100

    nameplates: revised module code; removed outdated options;

--- modules/nameplates.lua
+++ modules/nameplates.lua
@@ -7,7 +7,6 @@ local C, L, I = ns.LC.color, ns.L, ns.I
 
 -- module own local variables and local cached functions --
 -----------------------------------------------------------
-local NAMEPLATES = NAMEPLATES or "Nameplates"
 local name = "Nameplates" -- L["Nameplates"]
 local ttName,ttColumns,tt,createTooltip,module = name.."TT",5
 local nameplateStatus = {};
@@ -20,24 +19,20 @@ I[name] = {iconfile="Interface\\Addons\\"..addon.."\\media\\nameplates"}; --Icon
 
 -- some local functions --
 --------------------------
--- Function to get the Nameplate CVar status
-local function getCVarSettings(cVarName)
-	local shown
-	local toggle = 1
-	if GetCVar(cVarName) == "1" then
-		shown = C("white",L["Shown"])
-		toggle = 0
-	elseif GetCVar(cVarName) == nil then
-		RegisterCVar(cVarName, 0)
-		shown = C("gray",L["Hidden"])
-	else
-		shown = C("gray",L["Hidden"])
+local function toggleCVar(self,v)
+	ns.debug("<toggleCVar>",v.type,v.cvar,v.state);
+	if v.type=="single" then
+		ns.SetCVar(v.cvar, v.state and 0 or 1, v.cvar);
+	elseif v.type=="group" then
+		for cvar, value in pairs(v.cvar)do
+			if type(cvar)=="string" and  GetCVar(cvar)~=value then
+				ns.SetCVar(cvar, value, cvar);
+			end
+		end
 	end
-	return shown, toggle
-end
-
-local function ttSetCVar(self,info)
-	ns.SetCVar(unpack(info));
+	if v.cvar and type(v.onChange)=="function" then
+		v.onChange();
+	end
 	createTooltip(tt);
 end
 
@@ -52,154 +47,64 @@ function createTooltip(tt)
 	local depend, dependName,color = 3,"","gray"
 	local _, line, cell = nil,false,1
 
-	if ns.build>70000000 then
-		for _,row in ipairs(nameplateStatus)do
-			local l=tt:AddLine();
-			local cell,cellFunction = 1;
-			for i,v in ipairs(row) do
-				local color = "white";
-				if v.txt and v.txt:find("\124") then
-					v.txt=gsub(v.txt,"\124T.+\124t","");
-				end
-				v.txt = gsub(v.txt,"\n"," ");
-				if v.cvar then
-					local state,dstate;
-					if type(v.cvar)=="string" then
-						state = GetCVarBool(v.cvar);
-						if v.cvar_depend then
-							dstate = GetCVarBool(v.cvar_depend);
-						end
-						cellFunction=function()
-							ns.SetCVar(v.cvar, state and 0 or 1, v.cvar);
-							createTooltip(tt)
-						end;
-					elseif type(v.cvar)=="table" then
-						state = true;
-						if v.cvar[0] and v.cvar[1] then
-							for cvar, value in pairs(v.cvar[1])do
-								if type(cvar)=="string" and  GetCVar(cvar)~=value then
-									state = false;
-								end
-							end
-						else
-							for cvar, value in pairs(v.cvar)do
-								if type(cvar)=="string" and  GetCVar(cvar)~=value then
-									state = false;
-								end
-							end
-						end
-						if v.cvar_depend then
-							dstate = GetCVarBool(v.cvar_depend);
-						end
-						cellFunction=function()
-							if v.cvar[0] and v.cvar[1] then
-								for cvar, value in pairs(v.cvar[state and 0 or 1])do
-									if type(cvar)=="string" and  GetCVar(cvar)~=value then
-										ns.SetCVar(cvar, value, cvar);
-									end
-								end
-							else
-								for cvar, value in pairs(v.cvar)do
-									if type(cvar)=="string" and  GetCVar(cvar)~=value then
-										ns.SetCVar(cvar, value, cvar);
-									end
-								end
-							end
-							if type(v.onChange) then
-								v.onChange();
-							end
-							createTooltip(tt)
-						end;
-					end
-					if v.colors then
-						color = v.colors[state and 1 or 2];
-					else
-						color = state and "white" or "ltgray"
-					end
-					if dstate==false then
-						color = v.colors[state and 3 or 4];
-					end
-
-				elseif type(v.colors)=="string" then
-					color = v.colors;
-				end
-				tt:SetCell(l,cell,C(color,strtrim(v.txt)), nil, nil, v.rows);
-				if cellFunction then
-					tt:SetCellScript(l, cell, "OnMouseUp", cellFunction);
-				end
-				cell = cell + (v.rows or 1);
+	for _,row in ipairs(nameplateStatus)do
+		local l=tt:AddLine();
+		local cell = 1;
+		for i,v in ipairs(row) do
+			local color = "white";
+			if v.txt and v.txt:find("\124") then
+				v.txt=gsub(v.txt,"\124T.+\124t","");
 			end
-			if row.separator then
-				if row.separator==1 then
-					tt:AddSeparator();
-				else
-					tt:AddSeparator(row.separator,0,0,0,0);
+			v.txt = gsub(v.txt,"\n"," ");
+			if v.cvar then
+				v.state,v.dstate=nil,nil; -- cvar state, dependency cvar state
+				if v.type=="single" then
+					v.state = GetCVarBool(v.cvar);
+				elseif v.type=="group" then
+					v.state = true;
+					for cvar, value in pairs(v.cvar)do
+						if type(cvar)=="string" and  GetCVar(cvar)~=value then
+							v.state = false;
+						end
+					end
+				end
+				if v.cvar_depend then
+					v.dstate = GetCVarBool(v.cvar_depend);
+				elseif v.cvar_depend_inv then
+					v.dstate = not GetCVarBool(v.cvar_depend_inv);
 				end
 			end
+			local tColors = type(v.colors);
+			if tColors=="table" then
+				local cIndex = v.state and 1 or 2;
+				if v.dstate==false then
+					cIndex = cIndex+2;
+				end
+				color = v.colors[cIndex];
+			elseif tColors=="string" then
+				color = v.colors;
+			elseif v.state~=nil then
+				color = v.state and "white" or "ltgray";
+			end
+			tt:SetCell(l,cell,C(color,v.txt:trim()), nil, nil, v.rows);
+			if v.cvar then
+				tt:SetCellScript(l, cell, "OnMouseUp", toggleCVar, v);
+			end
+			cell = cell + (v.rows or 1);
 		end
-	else
-		for _, v in ipairs(nameplateStatus) do
-			if (type(v[1])=="string") and (v[1]:find("\124")) then
-				v[1]=gsub(v[1],"\124T.+\124t","");
-			end
-			if v[1]==true then
-				tt:AddSeparator(1,0,0,0,0) -- transparent
-				line = false
-			elseif type(v[1])=="string" and v[2]==nil then
-				line = tt:AddLine()
-				tt:SetCell(line, 1, C("ltblue",v[1]), nil, nil, ttColumns)
-				tt:AddSeparator()
-				line = false
-			elseif v[2] == false then
-				line = tt:AddLine()
-				tt:SetCell(line, 1, C("ltyellow",v[1]), nil, nil, 1)
-				cell = 2
-			elseif v[3]~=nil and type(v[3])=="number" then
-				local shown, toggle = getCVarSettings(v[2])
-				if cell>1 and cell>ttColumns then
-					line = false
-				end
-				if line == false then
-					cell = 1
-					line = tt:AddLine()
-				end
-				tt:SetCell(line, cell, C(toggle==1 and "gray" or "white",v[1]), nil, nil, v[3])
-				tt:SetCellScript(line, cell, "OnMouseUp", ttSetCVar, {v[2], toggle, v[2]});
-				cell = cell + v[3];
+		if row.separator then
+			if row.separator==1 then
+				tt:AddSeparator();
 			else
-				local shown, toggle = getCVarSettings(v[2])
-				if type(v[3])=="string" and dependName~=v[3] then
-					_, depend = getCVarSettings(v[3])
-					dependName = v[3]
-				elseif v[3]==nil then
-					depend, dependName = 3,""
-				end
-				if line == false then
-					cell = 1
-					line = tt:AddLine()
-				end
-				if v[3]==true then
-					tt:SetCell(line, 1, C(toggle==1 and "gray" or "white",v[1]), nil, nil, 5)
-					line = false
-				else
-					if depend == 0 then
-						color = toggle==1 and "gray" or "white"
-					elseif depend == 1 then
-						color = toggle==1 and "dkgray" or "gray"
-					elseif depend == 3 then
-						color = toggle==1 and "gray" or "white"
-					end
-					tt:SetCell(line, cell, C(color,v[1]), nil, nil, 1)
-					tt:SetCellScript(line, cell, "OnMouseUp", ttSetCVar, {v[2], toggle, v[2]});
-					cell = cell + 1
-				end
+				tt:AddSeparator(row.separator,0,0,0,0);
 			end
 		end
 	end
 
 	if ns.profile.GeneralOptions.showHints then
-		tt:AddLine(" ")
+		tt:AddSeparator(4,0,0,0,0);
 		tt:SetCell(tt:AddLine(), 1, C("ltblue",L["MouseBtn"]).." || "..C("green",L["Names/Nameplates on/off"]), nil, nil, ttColumns)
+		ns.ClickOpts.ttAddHints(tt,name);
 	end
 	ns.roundupTooltip(tt);
 end
@@ -220,122 +125,93 @@ module = {
 -- function module.options() return {} end
 
 function module.init()
+	ttColumns = 6;
+	local colors1,colors2,colors3 = {"white","gray"},{"white","gray","gray","dkgray"},{"dkgray","black","white","gray"};
+	local UNML = InterfaceOptionsNamesPanelUnitNameplatesMakeLarger
+	local UpdateNamePlateOptions = function() NamePlateDriverFrame:UpdateNamePlateOptions() end
 	nameplateStatus = {
-		{ FRIENDLY },
-		{ NAMES_LABEL, false },
-		{ PLAYER,						"UnitNameFriendlyPlayerName"},
-		{ UNIT_NAME_FRIENDLY_PETS,		"UnitNameFriendlyPetName", ""},
-		{ UNIT_NAME_FRIENDLY_GUARDIANS, "UnitNameFriendlyGuardianName", ""},
-		{ UNIT_NAME_FRIENDLY_TOTEMS,	"UnitNameFriendlyTotemName", ""},
-		{ true },
+		{{txt=PLAYER,colors="ltblue"}, {txt=FRIENDLY,colors="ltgreen",rows=2}, {txt=ENEMY,colors="ltred",rows=3}, separator=1},
+		{
+			{txt=NAMES_LABEL, colors="ltyellow" },
+			{txt=PLAYER,					type="single", cvar="UnitNameFriendlyPlayerName",	colors=colors1 },
+			{txt=UNIT_NAME_FRIENDLY_MINIONS,type="single", cvar="UnitNameFriendlyMinionName",	colors=colors2,	cvar_depend="UnitNameFriendlyPlayerName" },
+			{txt=PLAYER,					type="single", cvar="UnitNameEnemyPlayerName",		colors=colors1 },
+			{txt=UNIT_NAME_ENEMY_MINIONS,	type="single", cvar="UnitNameEnemyMinionName",		colors=colors2,	cvar_depend="UnitNameEnemyPlayerName" },
+		},
+		{
+			{txt=L["Nameplates"], colors="ltyellow" },
+			{txt=PLAYER,								type="single", cvar="nameplateShowFriends",			colors=colors1 },
+			{txt=UNIT_NAMEPLATES_SHOW_FRIENDLY_MINIONS,	type="single", cvar="nameplateShowFriendlyMinions",	colors=colors2,	cvar_depend="nameplateShowFriends" },
+			{txt=PLAYER,								type="single", cvar="nameplateShowEnemies",			colors=colors1 },
+			{txt=UNIT_NAMEPLATES_SHOW_ENEMY_MINIONS,	type="single", cvar="nameplateShowEnemyMinions",	colors=colors2,		cvar_depend="nameplateShowEnemies" },
+			{txt=UNIT_NAMEPLATES_SHOW_ENEMY_MINUS,		type="single", cvar="nameplateShowEnemyMinus",		colors=colors2,		cvar_depend="nameplateShowEnemies" },
+		},
 
-		{ L["Nameplates"], false },
-		{ PLAYER,						"nameplateShowFriends"},
-		{ UNIT_NAME_FRIENDLY_PETS,		"nameplateShowFriendlyPets", "nameplateShowFriends"},
-		{ UNIT_NAME_FRIENDLY_GUARDIANS, "nameplateShowFriendlyGuardians", "nameplateShowFriends"},
-		{ UNIT_NAME_FRIENDLY_TOTEMS,	"nameplateShowFriendlyTotems", "nameplateShowFriends"},
-		{ true },
+		{separator=4},
 
-		{ ENEMY },
-		{ NAMES_LABEL, false },
-		{ PLAYER,						"UnitNameEnemyPlayerName"},
-		{ UNIT_NAME_ENEMY_PETS,			"UnitNameEnemyPetName", ""},
-		{ UNIT_NAME_ENEMY_GUARDIANS,	"UnitNameEnemyGuardianName", ""},
-		{ UNIT_NAME_ENEMY_TOTEMS,		"UnitNameEnemyTotemName", ""},
-		{ true },
+		{{txt=UNIT_NAME_NPC,colors="ltblue"}, separator=1},
+		{
+			{txt=NPC_NAMES_DROPDOWN_ALL,type="single", cvar="UnitNameNPC",						colors=colors1 },
+			{txt=L["Interactive NPCs"],	type="single", cvar="UnitNameInteractiveNPC",			colors=colors2,	cvar_depend_inv="UnitNameNPC", rows=2},
+			{txt=L["Special NPCs"],		type="single", cvar="UnitNameFriendlySpecialNPCName",	colors=colors2,	cvar_depend_inv="UnitNameNPC", rows=2},
+			{txt=L["Hostile NPCs"],		type="single", cvar="UnitNameHostleNPC",				colors=colors2,	cvar_depend_inv="UnitNameNPC"},
+		},
 
-		{ L["Nameplates"], false },
-		{ PLAYER,						"nameplateShowEnemies"},
-		{ UNIT_NAME_ENEMY_PETS,			"nameplateShowEnemyPets", "nameplateShowEnemies"},
-		{ UNIT_NAME_ENEMY_GUARDIANS,	"nameplateShowEnemyGuardians", "nameplateShowEnemies"},
-		{ UNIT_NAME_ENEMY_TOTEMS,		"nameplateShowEnemyTotems", "nameplateShowEnemies"},
-		{ true },
+		{separator=4},
 
-		{ L["Misc"] },
-		{ UNIT_NAME_NONCOMBAT_CREATURE,	"UnitNameNonCombatCreatureName",	1},
-		{ UNIT_NAME_OWN,				"UnitNameOwn",						2},
-		{ UNIT_NAME_PLAYER_TITLE,		"UnitNamePlayerPVPTitle",			2},
-		{ UNIT_NAME_GUILD,				"UnitNamePlayerGuild",				1},
-		{ UNIT_NAME_GUILD_TITLE,		"UnitNameGuildTitle",				2},
-		{ UNIT_NAME_NPC,				"UnitNameNPC",						2},
-		{ SHOW_CLASS_COLOR_IN_V_KEY,	"ShowClassColorInNameplate",		1},
-		{ UNIT_NAME_HIDE_MINUS,			"UnitNameForceHideMinus",			4},
+		{{txt=L["More names"],	colors="ltblue" },separator=1},
+		{
+			{txt=UNIT_NAME_OWN,		type="single", cvar="UnitNameOwn", colors=colors1 },
+			{txt=UNIT_NAME_NONCOMBAT_CREATURE,	type="single", cvar="UnitNameNonCombatCreatureName", colors=colors1, rows=3},
+		},
+
+		{separator=4},
+
+		{{txt=L["More nameplate options"],	colors="ltblue" },separator=1},
+		{
+			{ txt=L["Nameplate size"], colors="dkyellow" },
+
+			{ txt=PLAYER_DIFFICULTY1,
+				colors=colors1,
+				onChange=UpdateNamePlateOptions,
+				type="group", cvar={NamePlateHorizontalScale=tostring(UNML.normalHorizontalScale),   NamePlateVerticalScale=tostring(UNML.normalVerticalScale)} -- 1, 1.4
+			},
+			{ txt=VIDEO_OPTIONS_FAIR,
+				colors=colors1,
+				onChange=UpdateNamePlateOptions,
+				type="group", cvar={NamePlateHorizontalScale="1.18", NamePlateVerticalScale="1.9"}
+			},
+			{ txt=LARGE,
+				colors=colors1,
+				onChange=UpdateNamePlateOptions,
+				type="group", cvar={NamePlateHorizontalScale=tostring(UNML.largeHorizontalScale), NamePlateVerticalScale=tostring(UNML.largeVerticalScale)} -- 1.4, 2.7
+			},
+			{ txt=L["Larger"], colors=colors1,
+				onChange=UpdateNamePlateOptions,
+				type="group", cvar={NamePlateHorizontalScale="1.9", NamePlateVerticalScale="3.8"}
+			},
+			{ txt=L["Even larger"], colors=colors1,
+				row=2,
+				onChange=UpdateNamePlateOptions,
+				type="group", cvar={NamePlateHorizontalScale="2.6", NamePlateVerticalScale="5.2"}
+			},
+		},
+		{
+			{txt=L["Nameplates out of combat"],  type="single", cvar="nameplateShowAll", colors=colors1, rows=1},
+			{txt=SHOW_NAMEPLATE_LOSE_AGGRO_FLASH,  type="single", cvar="ShowNamePlateLoseAggroFlash", colors=colors1, rows=4},
+		},
+
+		{separator=4},
+
+		{{txt=DISPLAY_PERSONAL_RESOURCE,colors="ltblue" },separator=1},
+		{{txt=DISPLAY_PERSONAL_RESOURCE,type="single", cvar="nameplateShowSelf", colors=colors1,rows=ttColumns}},
+		{{txt=DISPLAY_PERSONAL_RESOURCE_ON_ENEMY,type="single", cvar="nameplateResourceOnTarget", colors=colors1, rows=ttColumns}},
+
+		{separator=4},
+
+		{{txt=L["Misc"],colors="ltblue"},separator=1},
+		{{txt=L["Show quest unit circles"],type="single", cvar="ShowQuestUnitCircles", colors=colors1,rows=ttColumns}}
 	};
-	if ns.build>70000000 then -- Legion
-		ttColumns = 6;
-		nameplateStatus = {
-			{{txt=PLAYER,colors="ltblue"}, {txt=FRIENDLY,colors="ltgreen",rows=2}, {txt=ENEMY,colors="ltred",rows=3}, separator=1},
-			{
-				{txt=NAMES_LABEL, colors="ltyellow" },
-				{txt=PLAYER,						cvar="UnitNameFriendlyPlayerName"	},
-				{txt=UNIT_NAME_FRIENDLY_MINIONS,	cvar="UnitNameFriendlyMinionName",	colors={"white","gray","gray","dkgray"},	cvar_depend="UnitNameFriendlyPlayerName" },
-				{txt=PLAYER,						cvar="UnitNameEnemyPlayerName",		colors={"white","gray"} },
-				{txt=UNIT_NAME_ENEMY_MINIONS,		cvar="UnitNameEnemyMinionName",		colors={"white","gray","gray","dkgray"},		cvar_depend="UnitNameEnemyPlayerName" },
-			},
-			{
-				{txt=L["Nameplates"], colors="ltyellow" },
-				{txt=PLAYER,								cvar="nameplateShowFriends",		colors={"white","gray"} },
-				{txt=UNIT_NAMEPLATES_SHOW_FRIENDLY_MINIONS,	cvar="nameplateShowFriendlyMinions",colors={"white","gray","gray","dkgray"},	cvar_depend="nameplateShowFriends" },
-				{txt=PLAYER,								cvar="nameplateShowEnemies",		colors={"white","gray"} },
-				{txt=UNIT_NAMEPLATES_SHOW_ENEMY_MINIONS,	cvar="nameplateShowEnemyMinions",	colors={"white","gray","gray","dkgray"},		cvar_depend="nameplateShowEnemies" },
-				{txt=UNIT_NAMEPLATES_SHOW_ENEMY_MINUS,		cvar="nameplateShowEnemyMinus",		colors={"white","gray","gray","dkgray"},		cvar_depend="nameplateShowEnemies" },
-			},
-
-			{separator=4},
-
-			{{txt=UNIT_NAME_NPC,colors="ltblue"}, separator=1},
-			{
-				{txt=ALL,					cvar="UnitNameNPC" },
-				{txt=L["Interactive NPCs"],	cvar="UnitNameInteractiveNPC",			colors={"dkgray","black","white","gray"},	cvar_depend="UnitNameNPC", rows=2},
-				{txt=L["Special NPCs"],		cvar="UnitNameFriendlySpecialNPCName",	colors={"dkgray","black","white","gray"},	cvar_depend="UnitNameNPC", rows=2},
-				{txt=L["Hostle NPCs"],		cvar="UnitNameHostleNPC",				colors={"dkgray","black","white","gray"},	cvar_depend="UnitNameNPC"},
-			},
-
-			{separator=4},
-
-			{{txt=L["More names"],	colors="ltblue" },separator=1},
-			{
-				{txt=UNIT_NAME_OWN,		cvar="UnitNameOwn" },
-				{txt=UNIT_NAME_NONCOMBAT_CREATURE,	cvar="UnitNameNonCombatCreatureName", rows=3},
-			},
-
-			{separator=4},
-
-			{{txt=L["More nameplate options"],	colors="ltblue" },separator=1},
-			{
-				{ txt=C("ltyellow",L["Nameplate size"]) },
-				{ txt=PLAYER_DIFFICULTY1,
-					rows=2,
-					onChange=function() NamePlateDriverFrame:UpdateNamePlateOptions() end,
-					cvar={NamePlateHorizontalScale="1",   NamePlateVerticalScale="1"}
-				},
-				{ txt=LARGE,
-					rows=2,
-					onChange=function() NamePlateDriverFrame:UpdateNamePlateOptions() end,
-					cvar={NamePlateHorizontalScale="1.4", NamePlateVerticalScale="2.7"}
-				},
-				{ txt=L["Larger"],
-					onChange=function() NamePlateDriverFrame:UpdateNamePlateOptions() end,
-					cvar={NamePlateHorizontalScale="1.9", NamePlateVerticalScale="4.5"}
-				},
-			},
-			{
-				{txt=L["Nameplates out of combat"],  cvar="nameplateShowAll", rows=1},
-				{txt=SHOW_NAMEPLATE_LOSE_AGGRO_FLASH,  cvar="ShowNamePlateLoseAggroFlash", rows=4},
-			},
-
-			{separator=4},
-
-			{{txt=DISPLAY_PERSONAL_RESOURCE,colors="ltblue" },separator=1},
-			{{txt=DISPLAY_PERSONAL_RESOURCE,cvar="nameplateShowSelf",rows=ttColumns}},
-			{{txt=DISPLAY_PERSONAL_RESOURCE_ON_ENEMY,  cvar="nameplateResourceOnTarget", rows=ttColumns}},
-
-			{separator=4},
-
-			{{txt=L["Misc"],colors="ltblue"},separator=1},
-			{{txt=L["Show quest unit circles"],cvar="ShowQuestUnitCircles",rows=ttColumns}}
-		};
-	end
 end
 
 function module.onevent(self,event,msg,msg2)
