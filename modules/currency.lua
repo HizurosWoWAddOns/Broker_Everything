2f53f803bbed8a51dd088a37f1df93bb03111183
Hizuro <hizuro@gmx.net>
2019-05-29 20:06:08 +0200

    currency: added options to choose/change display currencies on broker button from option panel

--- modules/currency.lua
+++ modules/currency.lua
@@ -231,6 +231,38 @@ function createTooltip(tt,update)
 	ns.roundupTooltip(tt);
 end
 
+local function AceOptOnBroker(info,value)
+	local place=tonumber((info[#info]:gsub("currenciesInTitle","")));
+	if value~=nil then
+		local _,id = strsplit(":",value);
+		id = tonumber(id);
+		if id then
+			ns.profile[name].currenciesInTitle[place] = id;
+--@do-not-package@
+		else
+			ns.debug(name,info[#info],place,value,id);
+--@end-do-not-package@
+		end
+	end
+	return ("%05d"):format(ns.profile[name].currenciesInTitle[place]);
+end
+
+local function AceOptOnBrokerValues(info)
+	local place=tonumber(info[#info]:match("(%d+)$"));
+	local id = ns.profile[name].currenciesInTitle[place];
+	local list,n = {},1;
+	-- [=[
+	for i=#Currencies, 1, -1 do
+		local Name,_,Icon = GetCurrencyInfo(Currencies[i]);
+		if Name~="" then
+			list[("%04d:%05d"):format(n,Currencies[i])] = "|T"..Icon..":0|t "..Name;
+			n=n+1;
+		end
+	end
+	--]=]
+	return list;
+end
+
 
 -- module functions and variables --
 ------------------------------------
@@ -275,6 +307,13 @@ function module.options()
 			showCapBroker={ type="toggle", order=1, name=L["Show total/weekly cap"], desc=L["Display currency total cap in tooltip."] },
 			showCapColorBroker={ type="toggle", order=2, name=L["Coloring total/weekly cap"], desc=L["Display total/weekly caps in different colors"] },
 			spacer={ type="range", order=3, name=L["Space between currencies"], desc=L["Add more space between displayed currencies on broker button"], min=0, max=10, step=1 },
+--@do-not-package@
+			header={ type="header", order=4, name=L["Currency on broker"] },
+			currenciesInTitle1 = {type="select", order=5, name=L["CurrencyOnBrokerPlace1"], desc=L["CurrencyOnBrokerDesc"], values=AceOptOnBrokerValues, get=AceOptOnBroker, set=AceOptOnBroker },
+			currenciesInTitle2 = {type="select", order=6, name=L["CurrencyOnBrokerPlace2"], desc=L["CurrencyOnBrokerDesc"], values=AceOptOnBrokerValues, get=AceOptOnBroker, set=AceOptOnBroker },
+			currenciesInTitle3 = {type="select", order=7, name=L["CurrencyOnBrokerPlace3"], desc=L["CurrencyOnBrokerDesc"], values=AceOptOnBrokerValues, get=AceOptOnBroker, set=AceOptOnBroker },
+			currenciesInTitle4 = {type="select", order=8, name=L["CurrencyOnBrokerPlace4"], desc=L["CurrencyOnBrokerDesc"], values=AceOptOnBrokerValues, get=AceOptOnBroker, set=AceOptOnBroker },
+--@end-do-not-package@
 		},
 		tooltip = {
 			showTotalCap={ type="toggle", order=1, name=L["Show total cap"], desc=L["Display currency total cap in tooltip."] },
@@ -295,7 +334,7 @@ function module.OptionMenu(parent)
 
 	ns.EasyMenu:InitializeMenu();
 
-	ns.EasyMenu:AddEntry({ label=L["Currency in title - menu"], title=true});
+	ns.EasyMenu:AddEntry({ label=L["Currency on broker - menu"], title=true});
 
 	for place=1, BrokerPlacesMax do
 		local pList,pList2,d;
@@ -377,6 +416,7 @@ function module.init()
 		"EXPANSION_NAME3",416,615,614,361,
 		"EXPANSION_NAME2",241,61,
 		"EXPANSION_NAME1",1704,
+		--"HIDDEN_CURRENCIES",1599,1325,1506,1171,1703,1579,1602,1559,1600,1324,1541,1349,1347,1350,1592,1593,1594,1595,1598,1191,1596,1705,1714,1540,1501,1597,1585,1586,
 	};
 end
 
@@ -421,3 +461,30 @@ end
 -- final module registration --
 -------------------------------
 ns.modules[name] = module;
+
+--@do-not-package@
+if false then
+	-- collect function for "Currencies" table
+	function collect_currencies()
+		ns.data._currencies = {};
+		local n,h,o,t,_;
+		for i=1,GetCurrencyListSize()do
+			n,h=GetCurrencyListInfo(i);
+			if h then
+				-- search matching globalstrings
+				t={};
+				for k,v in pairs(_G)do
+					_,o=issecurevariable(_G,k);
+					if n==BE_DUNGEON_AND_RAID or (n==v and o==nil) then
+						tinsert(t,k);
+					end
+				end
+				n=t;
+			else
+				n=tonumber(tostring(GetCurrencyListLink(i)):match("currency:(%d+)"));
+			end
+			tinsert(ns.data._currencies,n);
+		end
+	end
+end
+--@end-do-not-package@
