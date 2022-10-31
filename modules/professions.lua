ed6dd01ac0fccce3ca815b8e99a44b460daa0a44
Hizuro <hizuro@gmx.net>
2022-10-31 22:28:29 +0100

    Professions: changed expansion skills tooltip

--- modules/professions.lua
+++ modules/professions.lua
@@ -327,46 +327,57 @@ local function AddFactionRecipeLines(tt,expansion,recipesByProfession)
 end
 
 local function expansionSkillLines_OnEnter(self,skillId)
-	tt2 = ns.acquireTooltip({ttName2, ttColumns2, "LEFT","RIGHT","CENTER"},{true,true},{self, "horizontal", tt});
-	--local content,expansion,tsId,recipes,factions = unpack(content);
-
-	--	expansionSkillLines = {-- C_TradeSkillUI.GetTradeSkillLineInfoByID
-	--
-
-	if expansionSkillLines[skillId] then
-		if C_TradeSkillUI.GetProfessionInfoBySkillLineID then
-			for i=1, #expansionSkillLines[skillId] do
-				local skillInfo = C_TradeSkillUI.GetProfessionInfoBySkillLineID(expansionSkillLines[skillId][i]);
-				if skillInfo then
-				local color,text = "ltgray",L["Learnable"];
-					if skillInfo.skillLevel and skillInfo.maxSkillLevel and skillInfo.maxSkillLevel~=0 then
-						local percentSkillLevel = skillInfo.skillLevel/skillInfo.maxSkillLevel;
-						if percentSkillLevel==1 then
-							color = "gray2","gray2";
-						else
-							color = "ffff"..string.format("%02x",255*percentSkillLevel).."00";
-						end
-						text = skillInfo.skillLevel..'/'..skillInfo.maxSkillLevel;
-					end
-					tt2:AddLine(C("ltyellow",skillInfo.professionName),C(color,text));
-				end
+	if not expansionSkillLines then
+		expansionSkillLines = {}
+		local list = C_TradeSkillUI.GetAllProfessionTradeSkillLines();
+		for i=1, #list do
+			local info = C_TradeSkillUI.GetProfessionInfoBySkillLineID(list[i]);
+			if info.parentProfessionID then
+				expansionSkillLines[info.parentProfessionID] = expansionSkillLines[info.parentProfessionID] or {};
+				tinsert(expansionSkillLines[info.parentProfessionID],list[i]);
 			end
-		else
-			for i=1, #expansionSkillLines[skillId] do
-				local skillLineDisplayName, skillLineRank, skillLineMaxRank, skillLineModifier, parentSkillLineID = C_TradeSkillUI.GetTradeSkillLineInfoByID(expansionSkillLines[skillId][i]);
-				if skillLineDisplayName then
-					local color,text = "ltgray",L["Learnable"];
-					if skillLineRank and skillLineMaxRank and skillLineMaxRank~=0 then
-						local skillPercent = skillLineRank/skillLineMaxRank;
-						if skillPercent==1 then -- on max skill
-							color = "gray2","gray2";
-						else
-							color = "ffff"..string.format("%02x",255*skillPercent).."00";
-						end
-						text = skillLineRank..'/'..skillLineMaxRank;
+		end
+	end
+
+	if not expansionSkillLines[skillId] then
+		--ns:debugPrint(skillId);
+		return
+	end
+
+	tt2 = ns.acquireTooltip({ttName2, ttColumns2, "LEFT","RIGHT","CENTER"},{true,true},{self, "horizontal", tt});
+
+	if C_TradeSkillUI.GetProfessionInfoBySkillLineID then
+		for i=1, #expansionSkillLines[skillId] do
+			local skillInfo = C_TradeSkillUI.GetProfessionInfoBySkillLineID(expansionSkillLines[skillId][i]);
+			if skillInfo then
+				local color,text = "ltgray",L["Learnable"];
+				if skillInfo.skillLevel and skillInfo.maxSkillLevel and skillInfo.maxSkillLevel~=0 then
+					local percentSkillLevel = skillInfo.skillLevel/skillInfo.maxSkillLevel;
+					if percentSkillLevel==1 then
+						color = "gray2","gray2";
+					else
+						color = "ffff"..string.format("%02x",255*percentSkillLevel).."00";
 					end
-					tt2:AddLine(C("ltyellow",skillLineDisplayName),C(color,text));
+					text = skillInfo.skillLevel..'/'..skillInfo.maxSkillLevel;
 				end
+				tt2:AddLine(C("ltyellow",skillInfo.expansionName),C(color,text));
+			end
+		end
+	else
+		for i=1, #expansionSkillLines[skillId] do
+			local skillLineDisplayName, skillLineRank, skillLineMaxRank, skillLineModifier, parentSkillLineID = C_TradeSkillUI.GetTradeSkillLineInfoByID(expansionSkillLines[skillId][i]);
+			if skillLineDisplayName then
+				local color,text = "ltgray",L["Learnable"];
+				if skillLineRank and skillLineMaxRank and skillLineMaxRank~=0 then
+					local skillPercent = skillLineRank/skillLineMaxRank;
+					if skillPercent==1 then -- on max skill
+						color = "gray2","gray2";
+					else
+						color = "ffff"..string.format("%02x",255*skillPercent).."00";
+					end
+					text = skillLineRank..'/'..skillLineMaxRank;
+				end
+				tt2:AddLine(C("ltyellow",skillLineDisplayName),C(color,text));
 			end
 		end
 	end
@@ -855,34 +866,6 @@ function module.init()
 	--]]
 
 	-- skillLineDisplayName, skillLineRank, skillLineMaxRank, skillLineModifier, parentSkillLineID = C_TradeSkillUI.GetTradeSkillLineInfoByID(skillLineID)
-
-	expansionSkillLines = {-- C_TradeSkillUI.GetTradeSkillLineInfoByID
-		-- Blacksmithing
-		[164] = {2477, 2476, 2475, 2474, 2473, 2472, 2454, 2437, 2751},
-		-- Tailoring
-		[197] = {2540, 2539, 2538, 2537, 2536, 2535, 2534, 2533, 2759},
-		-- Jewelcrafting
-		[755] = {2524, 2523, 2522, 2521, 2520, 2519, 2518, 2517, 2757},
-		-- Inscription
-		[773] = {2514, 2513, 2512, 2511, 2510, 2509, 2508, 2507, 2756},
-		-- Leatherworking
-		[165] = {2532, 2531, 2530, 2529, 2528, 2527, 2526, 2525, 2758},
-		-- Engineering
-		[202] = {2506, 2505, 2504, 2503, 2502, 2501, 2500, 2499, 2755},
-		-- Enchanting
-		[333] = {2494, 2493, 2492, 2491, 2489, 2488, 2487, 2486, 2753},
-		-- Alchemy
-		[171] = {2485, 2484, 2483, 2482, 2481, 2480, 2479, 2478, 2750},
-		-- Mining
-		[186] = {2572, 2571, 2570, 2569, 2568, 2567, 2566, 2565, 2761},
-		-- Herbalism
-		[182] = {2556, 2555, 2554, 2553, 2552, 2551, 2550, 2549, 2760}
-		-- fishing
-		--[356] = {},
-		-- cooking
-		--[185] = {},
-	}
-
 end
 
 function module.onevent(self,event,arg1,...)
